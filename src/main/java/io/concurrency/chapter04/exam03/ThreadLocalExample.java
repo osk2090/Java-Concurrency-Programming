f6621d3db0dbd7a59d401993c3f566e5bb9aaed8
osk2090 <osk2090@naver.com>
2025-08-23 11:59:51 +0900

    ``` feat: ThreadLocalExample 수정 및 기본값 설정 (issue: master)
    ThreadLocalExample 클래스를 수정하여 기본값을 설정하고
    스레드에서 각기 다른 값을 설정하고 출력하는 예시를 추가했습니다.
    - 스레드가 종료된 후 threadLocal 변수를 제거하는 로직을 추가하여
      기본값이 출력되는지 확인합니다.
    ```

--- src/main/java/io/concurrency/chapter04/exam03/ThreadLocalExample.java
+++ src/main/java/io/concurrency/chapter04/exam03/ThreadLocalExample.java
@@ -2,21 +2,24 @@ package io.concurrency.chapter04.exam03;
 
 public class ThreadLocalExample {
 
-  private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+//  private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+  private static ThreadLocal<String> threadLocal = ThreadLocal.withInitial(() -> "Hello World");
 
   public static void main(String[] args) {
     new Thread(() -> {
       System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
       threadLocal.set("스레드 1의 값");
       System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
+      threadLocal.remove();
+      System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get()); // remove하고 다시 찍어보면 기본값이 들어가 있음
     },"Thread-1").start();
 
     new Thread(() -> {
       System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
       threadLocal.set("스레드 2의 값");
       System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
+      threadLocal.remove();
+      System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
     },"Thread-2").start();
-
-
   }
 }
